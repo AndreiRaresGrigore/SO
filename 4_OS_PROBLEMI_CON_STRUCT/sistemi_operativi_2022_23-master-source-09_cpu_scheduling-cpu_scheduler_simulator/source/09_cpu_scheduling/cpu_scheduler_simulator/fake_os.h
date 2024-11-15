f047e848ae2c61e80eff565e63dc7f8147f67c70
AndreiRaresGrigore <grigore.1953878@studenti.uniroma1.it>
2024-11-15 16:52:11 +0100

    Completata parte main per cpu non in running e provato a portare alfa in fake_os

---
+++ 4_OS_PROBLEMI_CON_STRUCT/sistemi_operativi_2022_23-master-source-09_cpu_scheduling-cpu_scheduler_simulator/source/09_cpu_scheduling/cpu_scheduler_simulator/fake_os.h
@@ -0,0 +1,46 @@
+#include "fake_process.h"
+#include "linked_list.h"
+#pragma once
+
+
+typedef struct {
+  ListItem list;
+  int pid;
+  ListHead events;
+  ////////Entrambi calcolati in FakeOS_simStep dove c'è for
+  int burst_stimato; /////////Burst stimato dalla formula 
+  int burst_corrente;/////////Burst corrente 
+} FakePCB;
+
+struct FakeOS;
+typedef void (*ScheduleFn)(struct FakeOS* os, void* args);
+
+
+typedef struct {
+  float alfa;
+} SchedSJFArgs;
+
+typedef struct{
+  int id;
+  FakePCB* running;
+} Cpu; //ricordati che i nomi CPU e IO danno fastidio 
+
+
+typedef struct FakeOS{
+  SchedSJFArgs* arg_sched;
+  int cpu_count;
+  Cpu* cpus;
+  //FakePCB* running;
+  ListHead ready;
+  ListHead waiting;
+  int timer;
+  ScheduleFn schedule_fn;
+  void* schedule_args;
+
+  ListHead processes;
+} FakeOS;
+
+
+void FakeOS_init(FakeOS* os, int cpu_count);
+void FakeOS_simStep(FakeOS* os);
+void FakeOS_destroy(FakeOS* os);
